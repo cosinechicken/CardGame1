41ecd13fe9a8e3a5f0679b7168d3637083a1c936
cosinechicken <ah.blenden@gmail.com>
2019-04-29 17:06:45 -0700

    Finished up to slide 226
    Finished up to slide 226, but there is an error in WeatherRetriever.cs :/

---
+++ Lab6/Lab6/WeatherRetriever.cs
@@ -0,0 +1,24 @@
+﻿using Lab6.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    class WeatherRetriever
+    {
+        private string apiKey = "HFXQfGjUsgUGg1s2Yr6JJ";
+        private string secret = "UCYD8DGUwMzphF6bLFlsIvYVCc62UUspNF784qob";
+
+        public async Task<ObservationsRootObject> GetObservations()
+        {
+            HttpClient httpClient = new HttpClient();
+            string apiUrl = $"https://api.aerisapi.com/observations/Seattle,WA,US?client_id={apiKey}&client_secret={secret}";
+            string responseString = await httpClient.GetStringAsync(apiUrl);
+            ObservationsRootObject observations = JsonConvert.DeserializeObject<ObservationsRootObject>(responseString);
+            return observations;
+        }
+    }
+}
